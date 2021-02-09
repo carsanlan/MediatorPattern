7b66c2bf11ed7b0b0ea71146eeda60796c924d39
Jorge Jimenez Sanchez <jjsanchez@us.es>
2021-02-09 14:44:15 +0100

    Añadiendo Pruebas

---
+++ src/test/java/MediatorPattern/ValveTest.java
@@ -0,0 +1,24 @@
+package MediatorPattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ValveTest {
+
+	@Test
+	void testSetMediator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testOpen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClosed() {
+		fail("Not yet implemented");
+	}
+
+}
